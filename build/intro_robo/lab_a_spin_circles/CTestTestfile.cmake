f4a1678e20e7e1484de6a609db1773a53955b008
mahedjaved <mahed95@gmail.com>
2024-10-08 21:28:10 +0000

    initialises spin_in_circles.py folder

---
+++ build/intro_robo/lab_a_spin_circles/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mjubuntu/catkin_ws/src/intro_robo/lab_a_spin_circles
+# Build directory: /home/mjubuntu/catkin_ws/build/intro_robo/lab_a_spin_circles
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
